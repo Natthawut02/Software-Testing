373bd631f01e1ce820910d48d32c8a06df8a5b53
Natthawut Thanomkaew <natthawutthanomkaew@gmail.com>
2025-03-02 14:50:18 +0700

    Add alternatingCharacters function

---
+++ alternatingCharacters.py
@@ -0,0 +1,16 @@
+def alternatingCharacters(s):
+alternatingCharacters    deletions = 0
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            deletions += 1
+    return deletions
+
+if __name__ == '__main__':
+    import os
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input().strip())
+    for q_itr in range(q):
+        s = input()
+        result = alternatingCharacters(s)
+        fptr.write(str(result) + '\n')
+    fptr.close()
