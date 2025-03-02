464e2b8a8a20b900621e39b341c2f3c90f8a54ea
Natthawut Thanomkaew <natthawutthanomkaew@gmail.com>
2025-03-02 16:28:47 +0700

    Add gridChallenge test cases

--- gridChallenge.py
+++ gridChallenge.py
@@ -9,3 +9,25 @@ def gridChallenge(grid):
             if grid[row][col] < grid[row - 1][col]:
                 return "NO"
     return "YES"
+
+
+
+    
+
+ # Test Case
+import unittest
+
+class TestGridChallenge(unittest.TestCase):
+    def test_grid_challenge(self):
+        self.assertEqual(gridChallenge(["abc", "ade", "efg"]), "YES")
+        self.assertEqual(gridChallenge(["ebacd", "fghij", "olmkn", "trpqs", "xywuv"]), "YES")
+        self.assertEqual(gridChallenge(["mpxz", "abcd", "wlmf"]), "NO")
+        self.assertEqual(gridChallenge(["abc", "hjk", "mpq", "rtv"]), "YES")
+        self.assertEqual(gridChallenge(["a"]), "YES")
+        self.assertEqual(gridChallenge(["abc", "def", "ghi"]), "YES")
+        self.assertEqual(gridChallenge(["xyz", "abc", "def"]), "NO")
+        self.assertEqual(gridChallenge(["zzz", "zzz", "zzz"]), "YES")
+        self.assertEqual(gridChallenge(["zzz", "aaa", "bbb"]), "NO")
+
+if __name__ == '__main__':
+    unittest.main()
